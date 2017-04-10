63dab13f066806ee1121a9f37e5c9a7b4f0108a5
waterkh <pjclark@email.sc.edu>
2017-04-10 10:00:46 -0400

    First Commit - Test Scene - Minor Reality Shift Start
    + First commit. Getting everything I have pushed to the repository.
    + Test Scene - Test scene to test out different functionalities we want
    to integrate into the game.
    + Reality Shift - Set up an enum for two modes to switch from. Probably
    could make this switch on a boolean rather than cycle through an enum,
    but enum allows for more realities to shift from, just an idea.
    SteamPunk and Victorian are just placeholders.

---
+++ _Scripts/RealityShift.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RealityShift : MonoBehaviour {
+
+	public enum Realities {
+		steamPunk = 0,
+		victorian = 1,
+	};
+	public static Realities currentReality = Realities.steamPunk;
+	public Text CurrRealityText;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/// <summary>
+	/// Changes the reality.
+	/// Use: Used on the OnClick function of buttons. 0 = SteamPunk, 1 = Victorian for input parameters.
+	///		This is just for testing purposes, we will integrate better when the time comes.
+	/// </summary>
+	/// <param name="reality">Reality.</param>
+	public void ChangeReality(int reality)
+	{
+		switch(reality)
+		{
+		case 0:
+			currentReality = Realities.steamPunk;
+			CurrRealityText.text = "SteamPunk : 0";
+			break;
+		case 1:
+			currentReality = Realities.victorian;
+			CurrRealityText.text = "Victorian : 1";
+			break;
+		default:
+			currentReality = Realities.steamPunk;
+			CurrRealityText.text = "SteamPunk : 0";
+			break;
+		}
+		// TODO Add animations, sound change, etc.
+	}
+}
