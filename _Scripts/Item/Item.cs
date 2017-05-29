5bfc43792721830135532632df4321013cdfe4ec
Ethan Harmon <ethanharmon2018@gmail.com>
2017-05-29 19:57:43 -0400

    More additions to Item.cs, and cleaned up text.

--- _Scripts/Item/Item.cs
+++ _Scripts/Item/Item.cs
@@ -18,7 +18,8 @@ namespace Items {
 
     public string color {get; set;}
 
-    public Clothing (string aColor, string aName, double aValue, double aWeight) : base (aName, aValue, aWeight)) {
+    public Clothing (string aColor, string aName, double aValue,
+                       double aWeight) : base (aName, aValue, aWeight) {
         color = aColor;
     }
   }
@@ -29,18 +30,22 @@ namespace Items {
     public PotionType potionType {get; set;}
     public int potency {get; set;}  // How many points the potion restores by
 
-    public Potions (string aName, double aValue, double aWeight, PotionType type, int aPotency) : base (aName, aValue, aWeight) {
+    public Potions (string aName, double aValue, double aWeight,
+                      PotionType type, int aPotency) : base (aName,
+                      aValue, aWeight) {
         potionType = type;
         potency = aPotency;
     }
   }
   public class Weapons : Item {
 
-    public double damage {get; set;}
-    public double range {get; set;}     // Are these three stats suitable to be choices for upgrades?
-    public double speed {get; set;}
+    public double damage {get; set;}  //  Between 1.0 - 100.0
+    public double range {get; set;}   //  Between 1.0 - 25.0
+    public double speed {get; set;}   //  Betweem 1.0 - 100.0
+        // Are these three stats suitable to be choices for upgrades?
 
-    public Weapons (string aName, double aValue, double aWeight, double aDamage, double aRange, double aSpeed) : base (aName, aValue, aWeight) {
+    public Weapons (string aName, double aWeight, double aDamage, double aRange,
+                      double aSpeed) : base (aName, aWeight) {
         damage = aDamage;
         range = aRange;
         speed = aSpeed;
@@ -48,7 +53,27 @@ namespace Items {
   }
   public class PrimaryWeapon : Weapons {
 
-    public enum Abilities {}; //  TODO: figure out how to organize abilities tree/list/thingy
+    public enum Abilities {none, }; //  TODO: figure out how to organize abilities tree/list/thingy
     public Abilities abilities {get; set;}
+    public enum Effects {none, poison, slow, freeze}  //  TODO: decide on effects
+    public Effects effect {get; set;}
+    public PrimaryWeapon Gauntlets;
+    public PrimaryWeapon Axe;
+    public PrimaryWeapon Flail;
+
+    public PrimaryWeapon (string aName, double aWeight, double aDamage,
+                            double aRange, double aSpeed,
+                            Abilities someAbilities, Effects anEffect)
+                            : base (aName, aWeight, aDamage, aRange, aSpeed) {
+        someAbilities = abilities;
+        anEffect = effect;
+    }
+
+    PrimaryWeapon Gauntlets = new PrimaryWeapon("Gauntlets", 5.0, 5.0,
+                                                  1.0, 75.0, none, none);
+    PrimaryWeapon Axe = new PrimaryWeapon("Axe", 20.0, 15.0, 5.0, 40.0, none,
+                                            none);
+    PrimaryWeapon Flail = new PrimaryWeapon("Flail", 45.0, 30.0, 20.0, none,
+                                            none);
   }
 }
